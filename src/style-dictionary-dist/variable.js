f2c2c4d1d49cd52a1bab70c1799e75ab8c5784f0
vinodtj <mailvinodachary@gmail.com>
2022-09-24 16:23:37 +0400

    Create variable.js

---
+++ src/style-dictionary-dist/variable.js
@@ -0,0 +1,15 @@
+export const ColorBasePrimary = "#0c0745";
+export const ColorBaseSecondary = "#fc7aa5";
+export const ColorBackground = "#d8d8d8";
+export const ColorText = "#4422d7";
+export const FontFamilyBase =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif";
+export const FontFamilyCode =
+  "source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace";
+export const SizeTextSmall = "0.75rem";
+export const SizeTextMedium = "1.125rem";
+export const SizeTextBase = "1.125rem";
+export const SizeTextLarge = "2rem";
+export const SpacingSmall = "10px";
+export const SpacingMedium = "30px";
+export const SpacingLarge = "40px";
