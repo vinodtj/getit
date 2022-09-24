7d86d24b95a922473fbb90c64299481354f59918
vinodtj <mailvinodachary@gmail.com>
2022-09-24 16:27:52 +0400

    Update App.js
    removed two components

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import "./App.scss";
 
 // custom components for demo
 import ComponentWithSass from "./components/with-sass/ComponentWithSass";
-import ComponentWithStyledComponents from "./components/with-styled-components/ComponentWithStyledComponents";
-import ComponentWithCssModules from "./components/with-css-modules/ComponentWithCssModules";
 
 class App extends Component {
   render() {
